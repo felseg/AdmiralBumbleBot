6fbca9ab69b648c4a58ebe1333ae83e35be559ef
porksausages <porksausages@icloud.com>
2020-05-13 20:01:04 +0100

    removed debug code and changed formatting

--- src/commands/slap.rs
+++ src/commands/slap.rs
@@ -5,26 +5,27 @@ use serenity::{
 
 pub fn slap(ctx: &Context, msg: &Message, target: &str, args: &str) {
     let slapper = &msg.author.name;
-    println!("{}", target);
-    let slappee = UserId(
-        target
-            .parse()
-            .expect("Error parsing target"),
-    )
-    .to_user(&ctx.http)
-    .unwrap()
-    .name;
+    let slappee = UserId(target.parse().expect("Error parsing target"))
+        .to_user(&ctx.http)
+        .unwrap()
+        .name;
 
-    let message_text = if args.to_ascii_uppercase().starts_with(&['A', 'E', 'I', 'O', 'U'][..]) {
-        format!("*{} slaps {} in the face with an {}!*", slapper, slappee, args)
+    let message_text = if args
+        .to_ascii_uppercase()
+        .starts_with(&['A', 'E', 'I', 'O', 'U'][..])
+    {
+        format!(
+            "*{} slaps {} in the face with an {}!*",
+            slapper, slappee, args
+        )
     } else {
-        format!("*{} slaps {} in the face with a {}!*", slapper, slappee, args)
+        format!(
+            "*{} slaps {} in the face with a {}!*",
+            slapper, slappee, args
+        )
     };
 
     msg.channel_id
-        .say(
-            &ctx.http,
-            message_text,
-        )
+        .say(&ctx.http, message_text)
         .expect("Error sending message");
 }
