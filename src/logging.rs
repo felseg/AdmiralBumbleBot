5373403b6192d202de5476b918bdf27217ac97f6
Meemo <meemo4556@gmail.com>
2023-06-02 16:12:05 -0500

    Stop pinging users in logging channel

--- src/logging.rs
+++ src/logging.rs
@@ -2,7 +2,9 @@ use serenity::{model::id::ChannelId, prelude::Context};
 
 pub async fn log(ctx: &Context, message: &str) {
     if let Err(e) = ChannelId(get_env!("ABB_LOG_CHANNEL", u64))
-        .say(&ctx.http, message)
+        .send_message(&ctx.http, |msg| {
+            msg.content(message).allowed_mentions(|am| am.empty_parse())
+        })
         .await
     {
         eprintln!("Error sending message: {}", e);
